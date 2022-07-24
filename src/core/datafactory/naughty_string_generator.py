601a7a405b25b13117f0654b66048ada5493507a
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-07-24 12:19:43 +0800

    feat(datagen): Download all naughty-string files and and merge all into single List

--- src/core/datafactory/naughty_string_generator.py
+++ src/core/datafactory/naughty_string_generator.py
@@ -1,60 +1,22 @@
 from typing import List
 from storagemanager import StorageManager
 import pandas as pd
-import json
+import os
 
 class NaughtyStringGenerator:
     
+   
+    
     def __init__(self) -> None:
         self.sm = StorageManager()
+        self.blnsFileName = "blns.txt"
         
     def get_naughty_strings(self) -> pd.DataFrame:
         
+        naughtyStrings = self.get_all_naughty_strings()
         
-        self.get_all_naughty_strings()
-        
-        # blns = self.get_blns()
-        
-        # xss = self.get_xss_strings()
-        
-        # merged = blns + xss
-        
-        # return merged 
+        return naughtyStrings 
     
-    
-    def get_blns(self) -> List:
-        
-        try:
-        
-            blnsJson = self.sm.download_file_as_str('blns.txt', 'naughty-strings')
-            
-            decoded =  blnsJson.decode("utf-8") 
-            
-            strList = decoded.split("\r\n")
-            
-            nsList = [s for s in strList if not s.startswith("#")] #ignore comments in blns.txt
-            
-            return nsList
-        
-        except Exception as e:
-            # log with loguru
-            raise
-    
-    def get_xss_strings(self) -> List:
-        
-        try:
-            binaryData = self.sm.download_file_as_str('XSS-BruteLogic.txt', 'naughty-strings', 'xss')
-            
-            decoded =  binaryData.decode("utf-8")
-            
-            strList = decoded.split("\n")
-            
-            return strList
-        
-        except Exception as e:
-            # log with loguru
-            raise
-        
     def get_all_naughty_strings(self) -> List:
         
         fileNamePaths = self.sm.get_file_names_of_directory('naughty-strings/')
@@ -62,8 +24,45 @@ class NaughtyStringGenerator:
         if len(fileNamePaths) == 0:
             return []
         
+        merged = []
+        
         for fnp in fileNamePaths:
-            content = self.sm.download_file_as_str(fnp)
+            encodedContent = self.sm.download_file_as_str(fnp)
+            
+            decoded = encodedContent.decode('utf-8')
+            
+            splitted = []
+            
+            if self.is_blns_file(fnp):
+                splitted = self.handle_blns_content_splitting(decoded)
+            else:
+                splitted = decoded.split("\n")
+            
+            merged += splitted
+            
+        return merged
+            
+            
+            
+    def is_blns_file(self, fileNamePath):
+        
+        if os.path.basename(fileNamePath) == self.blnsFileName:
+            return True
+        return False
+            
+    def handle_blns_content_splitting(self, content):
+        splitted = content.split("\r\n")
+        
+        # remove comments and empty strings in seclist
+        cleansed = [x for x in splitted if not (x.startswith('#') and x != '')] 
+        
+        result = ['']
+        return result + cleansed
+        
+        
+    
+    def handle_seclist_content_splitting(self):
+        pass
         
         
         
