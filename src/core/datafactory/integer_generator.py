2417b4e7b31c210f0800047aca55a8eb93bf6b8f
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-07-24 21:38:45 +0800

    feat(datagen): FloatGenerator generates bad integers and random large floats

--- src/core/datafactory/integer_generator.py
+++ src/core/datafactory/integer_generator.py
@@ -1,7 +1,7 @@
 from storagemanager import StorageManager
 import pandas as pd
 import os
-from utils_list import ListUtils
+from data_factory_utils import DataFactoryUtils
 
 # integer data requirements
 # Seclist already contains various positive/negative integer data and a large 4MB interger
@@ -17,27 +17,21 @@ class IntegerGenerator:
     
     def generate_integers(self, noOfRowsToPad = 1000) -> list:
         
-        intData = self.load_integers_from_seclist()
+        # data contains some floats
+        intData = DataFactoryUtils.load_bad_integers_from_seclist()
+        
+        if noOfRowsToPad <= len(intData):
+            return intData
         
         rowsExcludeSuperLargeInt = noOfRowsToPad - 1
         
-        dupList = ListUtils.pad_rows(intData, rowsExcludeSuperLargeInt)
+        dupList = DataFactoryUtils.pad_rows(intData, rowsExcludeSuperLargeInt)
         
         # prepend super large int only once in the list to prevent taking up too much memory as 1 large integer is 4MB
         superLargeInt = self.load_superlarge_integer_from_seclist()
         dupList.insert(0, superLargeInt)
 
-        return dupList
-    
-    
-    def load_integers_from_seclist(self) -> list:
-        
-        #filePath = self.sm.get_file_names_of_directory()
-        content = self.sm.download_file_as_str('digit/numeric-fields-only.txt')
-        decoded = content.decode('utf-8')
-        splitted = decoded.split("\n")                  
-        return splitted
-    
+        return dupList    
     
     def load_superlarge_integer_from_seclist(self):
         
