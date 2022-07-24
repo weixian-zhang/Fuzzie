eb5d49d34a89d0b7e06328c6052580cd34ee756e
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-07-24 18:03:03 +0800

    feat(datagen): Generate and pad integer data where rows match number of rows in naughty-strings

--- src/core/datafactory/data_factory.py
+++ src/core/datafactory/data_factory.py
@@ -1,32 +1,65 @@
 
 from naughty_string_generator import NaughtyStringGenerator
+from integer_generator import IntegerGenerator
 import pandas as pd
 
 class FileData:
     fileName = ''
     content = ''
-       
-       
+
+class FileData:
+    name = ''
+    content = ''
+
+#Fuzzie uses Seclist(https://github.com/danielmiessler/SecLists) and in addition generate its own data 
 class DataFactory:
     
     def __init__(self) -> None:
 
-        self.df = pd.DataFrame()
+        # file column is of type FileData
+        self.df = pd.DataFrame({
+            'naughtystring': [],
+            'integer': [],
+            'float': [],
+            'datetime': [],
+            'chars': [],
+            'file': []
+        })
+        
         self.localDataDirectory = './' # directory location differs from clients. For e.g: vscode = vscode.workspace.workspaceFolders[0].uri.path
         self.fuzzdataDirectoryName = 'fuzzdata'
         self.dataRows = 10000 
     
     def generate_fuzz_dataset(self) -> pd.DataFrame:
-        df = self.generate_naughty_string()
-        return df
+        
+        nsdf = self.generate_naughty_string()
+        self.df['naughtystring'] = nsdf
+        
+        nsLength = len(nsdf)
+        
+        # generate and pad integer data to be equal rows to naughty-strings 
+        intdf = self.generate_integer(nsLength)
+        self.df['integer'] = intdf
+        
+        # debugging only
+        # for index, row in self.df.iterrows():
+        #     print(row['naughtystring'], row['integer'])
+        
+        return self.df
     
     def generate_naughty_string(self):
         
         sg = NaughtyStringGenerator()
         
-        ns = sg.get_naughty_strings()
+        df = sg.generate_naughty_strings()
         
-        self.df['naughtstrings'] = ns
+        return df
+        
+    def generate_integer(self, noOfRowsToPad) -> list:
+        
+        ig = IntegerGenerator()
+        intdf = ig.generate_integers(noOfRowsToPad)
+        return intdf
     
     def generate_char(self):
         pass
@@ -35,8 +68,6 @@ class DataFactory:
         bools = [True, False, None, 0, 1, 'true', 'false', 'yes', 'no', '1', '0', 't', 'f', 'T', 'F', 'TRUE', 'FALSE']
         return bools
     
-    def generate_integer(self):
-        pass
     
     def generate_float(self):
         pass
