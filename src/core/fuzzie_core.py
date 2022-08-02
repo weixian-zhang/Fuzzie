b1c7081ad346a24552140f74773f221c3da96cdd
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-02 22:33:02 +0800

    feat(fuzziecore): defined Fuzzie class api schema source input, draft class

--- src/core/fuzzie_core.py
+++ src/core/fuzzie_core.py
@@ -1,5 +1,35 @@
 
+import sys
 
+sys.path.insert(0, './api-initmanager')
+from openapi3_init_manager import OpenApi3ApiInitManager
+
+class TestCase:
+    data = {}
+    request = {}
+    response = {}
+    
+
+class Fuzzie:
+    
+    def __init__(self, openapiUrl = "", openapiFilePath="", fuzzieTextualApiFilePath="", textualApiString="") -> None:
+        openapiUrl: str = ""
+        openapiFilePath: str = ""
+        fuzzieTextualApiFilePath: str = ""
+        textualApiString: str = ""
+    
+    
+    def fuzz(self):
+        pass
+    
 #TODO
 
-# base on client input to execute correct init_manager
+# API schema source from client
+    # openapi3 url
+    # openapi3 file path
+    # .fuzzie file path - that describes api schema 
+    # textual api string
+    
+    # create ApiContext from api source
+    
+    # fuzzing - 
