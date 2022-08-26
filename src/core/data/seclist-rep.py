93c1310fc2524142d0a48908052ce85b4b3d1afc
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-26 12:12:49 +0800

    (seclist-prep): Add draft seclist-prep script and empty sqlite db

---
+++ src/core/data/seclist-rep.py
@@ -0,0 +1,14 @@
+
+#create all tables
+
+# CREATE TABLE [IF NOT EXISTS] VirusFile (
+# 	Name TEXT NOTNULL,
+# 	Content TEXT NOTNULL,
+# ) [WITHOUT ROWID];
+
+
+# download virus files
+    # eicar.txt and lottapixel.jpg | base64 , insert in db
+    
+# download rest of strings, digits, usernames, passwords
+    #insert into db
