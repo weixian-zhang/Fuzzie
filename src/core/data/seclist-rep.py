4a1534d311a8856f2ec7a570b81404c379959abd
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-08-26 15:58:20 +0800

    (data-prep): Add python script to load naughty files. - move all data-downloading python files into data-prep folder outside core/core

--- src/core/data/seclist-rep.py
+++ src/core/data/seclist-rep.py
@@ -1,14 +1,36 @@
+from naughty_file_generator import NaughtyFilesGenerator
+import pandas as pd
 
 #create all tables
 
-# CREATE TABLE [IF NOT EXISTS] VirusFile (
-# 	Name TEXT NOTNULL,
-# 	Content TEXT NOTNULL,
-# ) [WITHOUT ROWID];
+create_db_table_virusfile = '''
+CREATE TABLE [IF NOT EXISTS] VirusFile (
+	Name TEXT NOTNULL,
+	Content TEXT NOTNULL,
+) [WITHOUT ROWID];
+'''
+
+# files with metadata isLongFileName has no content but with very long file name to break systems that cannot
+# handle very long file names
+
+# exclude zipbombs
+
+create_db_table_seclist = '''
+CREATE TABLE [IF NOT EXISTS] SecList (
+	NaughtyString TEXT NOTNULL,
+	Digit TEXT NOTNULL,
+    UserName TEXT NOTNULL,
+    Password TEXT NOTNULL,
+) [WITHOUT ROWID];
+'''
 
 
-# download virus files
+# download virus files from Azure Storage. Cannot save as file on device as antimalware tool will block them
     # eicar.txt and lottapixel.jpg | base64 , insert in db
+
+naughtyFilesGen = NaughtyFilesGenerator()
+
+nFilesDF =  naughtyFilesGen.generate_naughty_files()
     
 # download rest of strings, digits, usernames, passwords
     #insert into db
