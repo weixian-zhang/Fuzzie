3beb762442230bc5fd793c533f7d40580e0e673b
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-10-03 11:39:23 +0800

    feat(datafactory-datetime): Generate valid and naughty date format

--- src/core/core/datafactory/naughty_datetime_generator.py
+++ src/core/core/datafactory/naughty_datetime_generator.py
@@ -1,5 +1,8 @@
 from datetime import date, time, datetime
+from random import random
 from dateutil import parser
+import time as tTime
+import random
 
 #datetime formats
 #https://medium.com/analytics-vidhya/dealing-with-date-time-of-different-formats-in-python-f1f973d8cdb
@@ -19,24 +22,60 @@ class NaughtyDateTimeGenerator(DataGenerator):
     
     def __init__(self):
         
+        super().__init__()
         
+        self.naughtyDateTimeSize = 50
         self.data = [
             date.min,
             date(1, 1, 1),
             date(9999, 12, 31),
+            date(1904, 1, 1)  ,  #mac system
+            date(1601, 1, 1) ,   #Windows system
+            tTime.mktime(datetime.now().timetuple()), #unix timestamp
             time(0, 0),
             time(23, 59, 59, 999999),
             datetime(1, 1, 1, 0, 0),
             datetime(9999, 12, 31, 23, 59, 59, 999999),
             datetime.now().date(),
-            datetime.now().time()
+            datetime.now().time(),
         ]
         
         self.generate_datetime_formats()
         
+        self.generate_naughty_datetime()
+        
     
     def generate_datetime_formats(self):
         
+        dateFormats = [
+            "%B %d, %Y %A, %H:%M:%S",
+            "%B %A %d %Y, %H:%M:%S",
+            "%A %B, %H:%M:%S",
+            "%d, %H:%M:%S",
+            "%d %B %A, %H:%M:%S",
+            "%d %A %B, %H:%M:%S",
+            "%d-%m-%y %H:%M:%S",
+            "%m-%d-%y %H:%M:%S",
+            "%y-%m-%d %H:%M:%S",
+            "%d/%m/%y %H:%M:%S",
+            "%m/%d/%y %H:%M:%S",
+            "%y/%m/%d %H:%M:%S",
+            "%y %m %d %H:%M:%S",
+            "%d %m %y %H:%M:%S",
+            "%m %d %y %H:%M:%S",
+            
+            #time only
+            "%H:%M:%S",
+            "%I:%M:%S",
+            "%H:%M:%S %f",
+            "%I:%M:%S %f",
+            ]
+        
+        for df in dateFormats:
+            newDF = datetime.now().strftime(df)
+            self.data.append(newDF)
+    
+    def generate_naughty_datetime(self):
         # %a: Weekday as locale’s abbreviated name. sun , mon
         # %A : Weekday as locale’s full name.Sunday, Monday, …
         # %w: Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
@@ -57,38 +96,25 @@ class NaughtyDateTimeGenerator(DataGenerator):
         # %j: Day of the year as a zero-padded decimal number.
         # %U: Week number of the year (Sunday as the first day of the week) as a zero
         
-        dateFormats = [
-            # "%B %d, %Y %A, %H:%M:%S",
-            #"%B %A %d %Y, %H:%M:%S",
-            #"%A %B, %H:%M:%S",
-            "%d, %H:%M:%S",
-            "%d %B %A, %H:%M:%S",
-            "%d %A %B, %H:%M:%S",
-            
-            "%d-%m-%y %H:%M:%S",
-            "%m-%d-%y %H:%M:%S",
-            "%y-%m-%d %H:%M:%S",
-            "%d/%m/%y %H:%M:%S",
-            "%m/%d/%y %H:%M:%S",
-            "%y/%m/%d %H:%M:%S",
-            
-            # "%d-%m-%Y %H:%M:%S",
-            # "%m-%d-%Y %H:%M:%S",
-            # "%Y-%m-%d %H:%M:%S",
-            # "%d/%m/%Y %H:%M:%S",
-            # "%m/%d/%Y %H:%M:%S",
-            # "%Y/%m/%d %H:%M:%S",
-            
-            #time only
-            "%H:%M:%S",
-            "%I:%M:%S",
-            "%H:%M:%S %f",
-            "%I:%M:%S %f",
-            ]
+        dtFormatTokens = ['%a','%A','%w','%d','%b','%B','%m','%y','%Y','%H','%I','%p','%M','%S','%f','%z','%Z','%j','%U']
         
-        for df in dateFormats:
-            newDF = datetime.now().strftime(df)
-            print(newDF)
+        currentSize = 0
+        
+        while currentSize <= self.naughtyDateTimeSize:
+        
+            for i, element in enumerate(dtFormatTokens):
+                nformat = f'''{dtFormatTokens[self.random_num(len(dtFormatTokens))]}, {dtFormatTokens[self.random_num(len(dtFormatTokens))]}, {dtFormatTokens[self.random_num(len(dtFormatTokens))]} {dtFormatTokens[self.random_num(len(dtFormatTokens))]} {dtFormatTokens[self.random_num(len(dtFormatTokens))]} {dtFormatTokens[self.random_num(len(dtFormatTokens))]}'''
+                nDateTime = newDF = datetime.now().strftime(nformat)
+
+                self.data.append(nDateTime)
             
-            newDFDateTime = parser.parse(newDF)
-            self.data.append(newDFDateTime)
+            currentSize += 1
+                
+    def random_num(self, dataLength):
+        return random.randint(0, dataLength - 1) #-1 for list 0 index
+                
+                
+    
+        
+            
+        
