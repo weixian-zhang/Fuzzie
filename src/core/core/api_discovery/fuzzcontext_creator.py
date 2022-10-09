2522738646a4191ef0d3498c67646926e0565751
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-10-09 14:24:48 +0800

    feat: api_discoverer supports parameters for POST operation instead only just GET operation

--- src/core/core/api_discovery/fuzzcontext_creator.py
+++ src/core/core/api_discovery/fuzzcontext_creator.py
@@ -2,8 +2,79 @@
 # how to serialize parameters
 # https://swagger.io/docs/specification/serialization/
 
+from sre_parse import fix_flags
+from apicontext_model import ApiContext, GetApi, MutatorApi, ApiVerb
+from fuzzcontext_model import ApiFuzzContext, FuzzMode, GetApiFuzzCaseSet
 
 class FuzzContextCreator:
     
-    def __init__(self, ) -> None:
+    def __init__(self, hostname: str, port: int, fuzzMode: str, 
+                 fuzzcaseToExec, apicontext: ApiContext,
+                 basicAuthnUserName = '', basicAuthnPassword = '',
+                 bearerTokenHeaderName = 'Authorization',
+                 bearerToken = '', 
+                 apikeyAuthnHeaderName = '',
+                 apikeyAuthnKey = ''):
+        
+        self.hostname = hostname
+        self.port = port
+        self.fuzzMode = fuzzMode
+        self.fuzzcaseToExec = fuzzcaseToExec 
+        self.apicontext = apicontext
+        
+        #security schemes
+        self.basicAuthnUserName = basicAuthnUserName
+        self.basicAuthnPassword = basicAuthnPassword
+        self.bearerTokenHeaderName = bearerTokenHeaderName
+        self.bearerToken = bearerToken
+        self.apikeyAuthnHeaderName = apikeyAuthnHeaderName
+        self.apikeyAuthnKey = apikeyAuthnKey
+        
+    def create_fuzzcontext(self):
+        
+        apis = self.apicontext.apis
+        
+        if apis == None or len(apis) == 0:
+            return ApiFuzzContext()
+        
+        
+        
+        for api in apis:
+            
+            if self.isGet(api.verb):
+                
+                getApi: GetApi  = api
+                
+                fuzzcaseSet = self.create_get_ApiFuzzCaseSet(getApi)
+                
+            
+                
+                
+    def isGet(self, verb):
+        if verb.lower() == ApiVerb.GET.value.lower():
+            return True
+        return False
+    
+    def isMutator(self, verb):
+        if (verb.lower() == ApiVerb.POST.value.lower() or
+            verb.lower() == ApiVerb.PATCH.value.lower() or
+            verb.lower() == ApiVerb.DELETE.value.lower() or
+            verb.lower() == ApiVerb.PUT.lower()):
+            return True
+        return False
+    
+    def create_fuzzcontext(self, apicontext: ApiContext):
         pass
+    
+    def create_get_ApiFuzzCaseSet(self, getapi: GetApi) -> GetApiFuzzCaseSet:
+        
+        fuzzcaseSet = GetApiFuzzCaseSet()
+        
+        fuzzcaseSet.selected = False
+        
+        
+        
+        
+        
+        
+    
--- src/core/core/api_discovery/fuzzcontext_model.py
+++ src/core/core/api_discovery/fuzzcontext_model.py
@@ -15,71 +15,103 @@ class FuzzMode(Enum):
 # describes a HTTP request header, Url and/or body with hydrated with seclist data 
 # A materialized request info object ready to make a HTTP call with
 class ApiFuzzRequest:
-    url: str = ""
-    headers = {}
-    body = {}       # send as Json string 
+    def __init__(self) -> None:  
+        self.url: str = ""
+        self.headers = {}
+        self.body = {}       # send as Json string 
 
 class ApiFuzzResponse:
-    httpVersion: str = ""
-    statusCode: str = ""
-    headers = {}
-    body = str      # Json string   
-    error: str = ""
+    def __init__(self) -> None:  
+        self.httpVersion: str = ""
+        self.statusCode: str = ""
+        self.headers = {}
+        self.body = str      # Json string   
+        self.error: str = ""
 
 # each "fuzz data set" is one ApiFuzzCase
 class ApiFuzzDataCase:
-    id: str = ""
-    data = {}
-    request: ApiFuzzRequest = {}
-    response: ApiFuzzResponse = {}
-    state: FuzzProgressState = FuzzProgressState.NOTSTARTED
+    def __init__(self) -> None:  
+        self.id: str = ""
+        self.data = {}
+        self.request: ApiFuzzRequest = {}
+        self.response: ApiFuzzResponse = {}
+        self.state: FuzzProgressState = FuzzProgressState.NOTSTARTED
 
-class ApiFuzzCaseSet:    
-    id: str = ""
-    selected: bool = True
-    path: str = ''        # path includes querystring
-    verb: ApiVerb = ApiVerb.GET
-    authnType: SupportedAuthnType = SupportedAuthnType.Anonymous
-    fuzzDataCases: list[ApiFuzzDataCase] = []
+class ApiFuzzCaseSet:
+    def __init__(self) -> None:  
+        self.id: str = ""
+        self.selected: bool = True
+        self.path: str = '' 
+        self.querystring: str = ''
+        self.verb: ApiVerb = ApiVerb.GET
+        self.authnType: SupportedAuthnType = SupportedAuthnType.Anonymous
+        self.fuzzDataCases: list[ApiFuzzDataCase] = []
     
-    #* important property
-    #all ApiFuzzDataCase generate new data base on this template. This template is a json string with 
-    # property name and value as data-type. Base on data-type fuzzer calls DataFactory.{data type} to generate tjhe correct
-    # fuzz data for that data-type
-    paramDataTemplate = {}                      
+        # all ApiFuzzDataCase generate new data base on this template. This template is a json string with 
+        # property name and value as data-type. Base on data-type fuzzer calls DataFactory.{data type} to generate tjhe correct
+        # fuzz data for that data-type
+        self.paramDataTemplate = {}                      
     
 #each set is a unque http verb + path
-class GetApiFuzzCaseSet(ApiFuzzCaseSet):     
-    paramIn: str = 'path'
-    paramProperties = list[ParamProp]
-    verb: ApiVerb = ApiVerb.POST
+class GetApiFuzzCaseSet(ApiFuzzCaseSet): 
+    def __init__(self) -> None:    
+        self.paramIn: str = 'path'
+        self.paramProperties = list[ParamProp]
+        self.verb: ApiVerb = ApiVerb.POST
     
-class MutatorApiFuzzCaseSet(ApiFuzzCaseSet):     
-    bodyParamProperties: list[ParamProp] = []
-    verb: ApiVerb = ApiVerb.GET
+class MutatorApiFuzzCaseSet(ApiFuzzCaseSet):
+    def __init__(self) -> None:
+        self.bodyParamProperties: list[ParamProp] = []
+        self.verb: ApiVerb = ApiVerb.GET
     
+class ApiAuthnBasic:
+    def __init__(self) -> None:
+        self.username = ''
+        self.password = ''
     
+class ApiAuthnBearerToken:
+    def __init__(self) -> None:
+        self.token = ''
+    
+class ApiAuthnApiKey:
+    def __init__(self) -> None:
+        self.headerName = ''
+        self.apikey = ''
+    
+class ApiAuthnApiKeyCookie:
+    
+    def __init__(self) -> None:
+        self.cookieName = ''
+        self.cookieValue = ''
+        
+class SecuritySchemes:
+    
+    def __init__(self) -> None:
+        self.authnType : SupportedAuthnType = SupportedAuthnType.Anonymous
+        self.basicAuthn: ApiAuthnBasic = None
+        self.bearerTokenAuthn: ApiAuthnBearerToken = None
+        self.apikeyAuthn: ApiAuthnApiKey = None
+        self.apikeyCookieAuthn: ApiAuthnApiKeyCookie = None
+          
 class ApiFuzzReport:
-    host: str   #domain or IP include port if not default 443/80
-    title: str
-    requiredAuthnTypes: list[str] = []
-    fuzzcaseSet: list[ApiFuzzCaseSet] = []
+    def __init__(self) -> None:
+        self.host: str   #domain or IP include port if not default 443/80
+        self.title: str
+        self.requiredAuthnTypes: list[str] = []
+        self.fuzzcaseSet: list[ApiFuzzCaseSet] = []
+
 
 # Also the data to be rendered on Fuzzie GUI client - VSCode extension and future Desktop client. 
 class ApiFuzzContext:
     
     def __init__(self) -> None:
         
-        # self.openapiUrl : str = ""
-        # self.openapiFilePath : str = ""
-        # self.requestTextFilePath : str = ""
-        # self.requestTextSingle : str = ""
-        
         self.hostname: str = ""
         self.port: int
         self.fuzzMode: FuzzMode = FuzzMode.Quick         
         self.fuzzcaseToExec = 50              # default 50
         self.apiFuzzCaseSet: list[ApiFuzzCaseSet] = []
+        self.securitySchemes: SecuritySchemes = SecuritySchemes()
         
         self.determine_fuzzcases()
         
@@ -96,8 +128,6 @@ class ApiFuzzContext:
             if self.fuzzcaseToExec <= 0:     # default to 50 if no value
                 self.fuzzcaseToExec = 50
             
-        
-    
 
     
 # Used by GUI clients to update fuzzing progress on each API
