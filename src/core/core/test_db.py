b1932890168111e63c701f487e725d3208d596de
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-10-15 12:26:09 +0800

    feat: Add FuzzContextSet table in SqlAlchemy ORM

--- src/core/core/test_db.py
+++ src/core/core/test_db.py
@@ -1,12 +1,12 @@
 import unittest
-from db import apifuzzcontext_TableName, api_fuzzcontex_table, dbconn
+from db import api_fuzzcontext_table, dbconn
 from sqlalchemy.sql import select
 
 class TestFuzzManager(unittest.TestCase):
     
         
     def test_query_apifuzzcontext(self):
-        query = select([api_fuzzcontex_table])
+        query = select([api_fuzzcontext_table])
         results = dbconn.execute(query)
         results.fetchall()
     
