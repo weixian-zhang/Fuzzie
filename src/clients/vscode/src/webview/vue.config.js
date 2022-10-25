00c86068df4d01d234586ed11ceabe1a391d46ee
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-10-25 10:59:13 +0800

    feat: Vuejs can boe loaded in webview

--- src/clients/vscode/src/webview/vue.config.js
+++ src/clients/vscode/src/webview/vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path');
 module.exports = defineConfig({
-  disableHostCheck: true,
   filenameHashing: false,
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, "../../vuejs-dist")
