47e2525129cd40bce304c37700609d9e8f84e0b4
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-10-26 17:46:36 +0800

    feat: Add Bootstrap and Master page layout to webview

--- src/clients/vscode/src/webview/src/components/HelloWorld.vue
+++ src/clients/vscode/src/webview/src/components/HelloWorld.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="hello">
-    <img alt="Mario" :src="require('../assets/img/mario.png')" />
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
---
+++ src/clients/vscode/src/webview/src/components/Master.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body">Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.Test Content with a lot of stuff.</div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body">Test Content</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body">Test Content</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body">Test Content</div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body">Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.Test Content with some stuff.</div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+  
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+  
+  @Options({
+    components: {
+      
+    },
+  })
+  export default class Master extends Vue {}
+  </script>
