cf7dd655c9dfcd9d332c84f3b847dd21f9d727e1
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-11-27 15:59:12 +0800

    feat(fuzzer-datetime-corpora): Add DateTimeCorpora

--- src/core/core/corporafactory/datetime_corpora.py
+++ src/core/core/corporafactory/datetime_corpora.py
@@ -1,7 +1,20 @@
 from dateutil import parser
 import time as tTime
 from datetime import date, time, datetime
+from dateutil import parser
+import datetime
 from corpora_mutator import CorporaMutator
+import random
+
+import os, sys
+from pathlib import Path
+currentDir = os.path.dirname(Path(__file__))
+sys.path.insert(0, currentDir)
+core_core_dir = os.path.dirname(Path(__file__).parent)
+sys.path.insert(0, core_core_dir)
+
+from eventstore import EventStore
+
 
 class DateTimeCorpora:
     
@@ -13,6 +26,11 @@ class DateTimeCorpora:
     def __init__(self) -> None:
         super().__init__()
         
+        self.dateCursor = 1
+        self.timeCursor = 1
+        
+        self.es = EventStore()
+        
         self.mutator = CorporaMutator()
         
         self.timeCorpora = {}
@@ -21,32 +39,107 @@ class DateTimeCorpora:
         
     def load_corpora(self):
         self.load_date_corpora()
+        self.load_time_corpora()
     
     
     def load_date_corpora(self):
         
-        # '1', date.min,
-        #     '2', date(1, 1, 1),
-        #     '3', date(9999, 12, 31),
-        #     '4', date(1904, 1, 1)  ,  #mac system
-        #     '5', date(1601, 1, 1) ,   #Windows system
-        #     '6', tTime.mktime(datetime.now().timetuple()), #unix timestamp
-        #     '7', time(0, 0),
-        #     '8', time(23, 59, 59, 999999),
-        #     '9', datetime(1, 1, 1, 0, 0),
-        #     '10', datetime(9999, 12, 31, 23, 59, 59, 999999),
-        #     '11', datetime.now().date(),
-        #     '12', datetime.now().time()
-        
-        dateFormats = ['%a','%A','%w','%d','%b','%B','%m','%y','%Y']
-        
-        dateFPerms = self.mutator.permutation(dateFormats, 0, len(dateFormats))
-        
-        i = 13
-        for dt in dateFPerms:
-            strFormat =  " ".join(dt)
-            dtVal = datetime.now().strftime(strFormat)
-            self.dateCorpora[str(i)] = dtVal
+        try:
+            self.dateCorpora['1'] = date.min
+            self.dateCorpora['2'] = date(1, 1, 1)
+            self.dateCorpora['3'] = date(9999, 12, 31)
+            self.dateCorpora['4'] = date(1601, 1, 1)
+            self.dateCorpora['5'] = tTime.mktime(datetime.datetime.now().timetuple())
+            self.dateCorpora['6'] = time(0, 0)
+            self.dateCorpora['7'] = time(23, 59, 59, 999999)
+            self.dateCorpora['8'] = datetime.datetime(1, 1, 1, 0, 0)
+            self.dateCorpora['9'] = datetime.datetime(9999, 12, 31, 23, 59, 59, 999999)
+            self.dateCorpora['10'] = datetime.datetime.now().date()
+            self.dateCorpora['11'] = datetime.datetime.now().time()
             
-            i = i + 1
+            dateFormats = ['%a','%A','%w','%d','%b','%B','%m','%y']
+            
+            dateFPerms = self.mutator.permutation(dateFormats, 0, len(dateFormats))
+            
+            i = 12
+            for dt in dateFPerms:
+                strFormat =  " ".join(dt)
+                
+                randYear = random.randrange(1900, 2100)
+                randMth = random.randrange(1, 12)
+                randDay = random.randrange(1, 31)
+                
+                try:
+                    dtVal = datetime.datetime(randYear, randMth, randDay).strftime(strFormat)
+                except:
+                    # error thrown for day out-of-range fro certain months without 29/30/31
+                    continue
+                
+                self.dateCorpora[str(i)] = dtVal
+                
+                i = i + 1
+        except Exception as e:
+            self.es.emitErr(e)
+            
+    def load_time_corpora(self):
+        
+        try:
+            i = 1
+            
+            timeFormats = ['%H','%I','%p','%M','%S','%f']
+        
+            timeFPerms = self.mutator.permutation(timeFormats, 0, len(timeFormats))
+            
+            for t in timeFPerms:
+                
+                randYear = random.randrange(1900, 2100)
+                randMth = random.randrange(1, 12)
+                randDay = random.randrange(1, 31)
+                randH = random.randrange(1, 12)
+                randM = random.randrange(1, 59)
+                randSec = random.randrange(1, 59)
+                
+                strFormat =  " ".join(t)
+                
+                tVal = datetime.datetime(randYear, randMth, randDay, randH, randM, randSec).strftime(strFormat)
+                
+                self.timeCorpora[str(i)] = tVal
+                
+                i = i + 1
+
+        except Exception as e:
+            self.es.emitErr(e)
+        
+       
+            
+    
+    def next_date_corpora(self):
+            
+        if self.dateCursor > (len(self.dateCorpora) - 1):
+            self.dateCursor = 1
+            
+        data = self.dateCorpora[str(self.dateCursor)]
+        
+        self.dateCursor += 1
+        
+        return data
+    
+    def next_time_corpora(self):
+            
+        if self.timeCursor > (len(self.timeCorpora) - 1):
+            self.timeCursor = 1
+            
+        data = self.timeCorpora[str(self.timeCursor)]
+        
+        self.timeCursor += 1
+        
+        return data
+    
+    def next_datetime_corpora(self):
+            
+        d = self.next_date_corpora()
+        t = self.next_time_corpora()
+        return f'{d} {t}'
+        
+        
         
--- src/core/core/corporafactory/test_datetime_corpora.py
+++ src/core/core/corporafactory/test_datetime_corpora.py
@@ -3,15 +3,28 @@ from datetime_corpora import DateTimeCorpora
 
 class TestPasswordCorpora(unittest.TestCase):
     
-    def test_generate_password_corpora(self):
-        
-        g = DateTimeCorpora()
-        g.load_corpora()
+    def setUp(self):
+         self.dtCorpora = DateTimeCorpora()
+         self.dtCorpora.load_corpora()
+   
+    
+    def test_generate_time_corpora(self):
+
+        for x in range(0, 50):
+            val = self.dtCorpora.next_time_corpora()
+            self.assertIsNotNone(val)
+            
+    def test_generate_date_corpora(self):
+    
+        for x in range(0, 50):
+            val = self.dtCorpora.next_date_corpora()
+            self.assertIsNotNone(val)
+            
+    def test_generate_datetime_corpora(self):
         
         for x in range(0, 50):
-            val = g.next_corpora()
+            val = self.dtCorpora.next_datetime_corpora()
             self.assertIsNotNone(val)
-            self.assertTrue(isinstance(val, str))
         
         
 if __name__ == '__main__':
