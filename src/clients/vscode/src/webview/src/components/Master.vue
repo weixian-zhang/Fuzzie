70a3732db0b73959d404416f29cd2ac3254948d0
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-12-13 09:53:19 +0800

    refactor(webview): increase Splitter gutter size

--- src/clients/vscode/src/webview/src/components/Master.vue
+++ src/clients/vscode/src/webview/src/components/Master.vue
@@ -1,59 +1,27 @@
 <template>
-    
-    
-      
-      
-        
-          <div class="container-fluid h-100 d-flex flex-column">
-            <Toast />
-          <Splitter style="height: 100%">
-            <SplitterPanel :size="100">
-              <Splitter layout="vertical">
-                <SplitterPanel :size="40">
-                  <Splitter>
-                    <SplitterPanel class="flex align-items-center justify-content-center" :size="25">
-                      <ApiDiscovery :vscodeMsger="vscodeMsger" :eventemitter="eventemitter" />
-                    </SplitterPanel>
-                    <SplitterPanel class="flex align-items-center justify-content-center" :size="75">
-                      <FuzzCaseSetPanel  :eventemitter="eventemitter" />
-                    </SplitterPanel>
-                  </Splitter>
-                </SplitterPanel>
-                <SplitterPanel class="flex align-items-center justify-content-center" :size="60">
-                  <FuzzResultPanel />
-                </SplitterPanel>
-                
-              </Splitter>
-            </SplitterPanel>
-          </Splitter>
-        </div>
-        
-    
-    <!-- <div class="container-fluid h-100 d-flex flex-column">
-    <Splitter layout="vertical" class="row h-50" :size="100">
-            <SplitterPanel :size="40">
-                 <Splitter layout="horizontal">
-                    <SplitterPanel class="col-3" :size="30">
-                    
-                          <ApiDiscovery :vscodeMsger="vscodeMsger" :eventemitter="eventemitter" />
-                      
-                    </SplitterPanel>
-                    <SplitterPanel class="col-9" :size="70">
-                    
-                          <FuzzCaseSetPanel  :eventemitter="eventemitter" />
-                      
-                    </SplitterPanel>
-                </Splitter>
-            </SplitterPanel>
-           
-          <class="row flex-fill d-flex"> -->
-            <!-- <SplitterPanel 
-                  <FuzzResultPanel />
-            </SplitterPanel>
-         
-
-          </Splitter> 
-          </div> -->
+  <div class="container-fluid h-100 d-flex flex-column">
+    <Toast />
+    <Splitter style="height: 100%" >
+      <SplitterPanel :size="100">
+        <Splitter layout="vertical" gutterSize="5">
+          <SplitterPanel :size="40" >
+            <Splitter gutterSize="5">
+              <SplitterPanel class="flex align-items-center justify-content-center" :size="25" >
+                <ApiDiscovery :vscodeMsger="vscodeMsger" :eventemitter="eventemitter" />
+              </SplitterPanel>
+              <SplitterPanel class="flex align-items-center justify-content-center" :size="75">
+                <FuzzCaseSetPanel  :eventemitter="eventemitter" />
+              </SplitterPanel>
+            </Splitter>
+          </SplitterPanel>
+          <SplitterPanel class="flex align-items-center justify-content-center" :size="60">
+            <FuzzResultPanel />
+          </SplitterPanel>
+          
+        </Splitter>
+      </SplitterPanel>
+    </Splitter>
+  </div>
 </template>
   
 <script lang="ts">
