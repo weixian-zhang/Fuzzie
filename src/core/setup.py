56c6b243a7fc5f79c3fc972adddf03907450b342
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-12-28 14:59:02 +0800

    fix(fuzzer-shiv-packager): exclude pyinstaller related modules which prevented fuzzer startup when running fuzzie-fuzzer.pyz

--- src/core/setup.py
+++ src/core/setup.py
@@ -1,16 +1,28 @@
 
 
 from setuptools import setup, find_packages
+
 import os
 
 sqlitedb = os.getcwd() + "\core\corporafactory\data\\fuzzie.sqlite"
 # localhost_crt = os.getcwd() + "\core\certs\localhost+2.pem"
 # localhost_key= os.getcwd() + "\core\certs\localhost+2-key.pem"
 
+import os
+
+required_modules = []
+modules_to_exclude = ['pyinstaller==5.1', 'pyinstaller-hooks-contrib']
 with open('core/requirements.txt', 'rb') as f:
-    required = f.read()
-    required = required.decode("utf-16")
-    print(required)
+    decode = f.read().decode("utf-16")
+    
+    splitted = decode.split('\r\n')
+    for fn in splitted:
+        if fn not in modules_to_exclude:
+            required_modules.append(fn)
+        
+    # required = f.read()
+    # required = required.decode("utf-16")
+    # print(required)
     
 setup(
    name='Fuzzie',
@@ -22,8 +34,19 @@ setup(
    author_email='',
    url="",
    data_files=[sqlitedb], #, localhost_crt, localhost_key],     #contains sqlite.db and other data files
-   packages=find_packages(),  #same as name
-   install_requires=required  #external packages as dependencies
+   packages=find_packages(
+       exclude=[
+           "tests",
+           "build",
+           "dist",
+           ".git",
+           ".gitignore",
+           "Fuzzie.egg-info"
+           ]
+       
+    ),  #same as name
+   install_requires=required_modules #required  #external packages as dependencies
+   
 )
 
 
