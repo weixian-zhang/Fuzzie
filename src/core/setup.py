0df1b7f2da8de4d8913a5769bcd8a6bfcb3f1054
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2023-05-31 09:32:08 +0800

    refactor: exclude sqlite db specifically in setup.py as Shiv already handles it

--- src/core/setup.py
+++ src/core/setup.py
@@ -3,43 +3,36 @@ from setuptools import setup, find_packages
 from pathlib import Path
 import os
 
-sqlitedb = os.getcwd() + "\core\corporafactory\data\\fuzzie.sqlite"
-# localhost_crt = os.getcwd() + "\core\certs\localhost+2.pem"
-# localhost_key= os.getcwd() + "\core\certs\localhost+2-key.pem"
-
 curDir = os.path.dirname(Path(__file__))
 requirementTxtPath = os.path.join(curDir, 'core', 'requirements.txt')
 
 required_modules = []
-modules_to_exclude = ['pyinstaller==5.1', 'pyinstaller-hooks-contrib', 'pylint']
 
 with open(requirementTxtPath, 'rb') as f:
     decode = f.read().decode("utf-8")
     
     splitted = decode.split('\r\n')
     for fn in splitted:
-        if fn not in modules_to_exclude:
-            required_modules.append(fn)
+        required_modules.append(fn)
     
 setup(
    name='Fuzzie',
-   version='0.16.0',
    description='',
    license="MIT",
-   long_description='',
    author='WXZ',
-   author_email='',
-   url="",
-   data_files=[sqlitedb], #, localhost_crt, localhost_key],     #contains sqlite.db and other data files
    packages=find_packages(
        exclude=[
            "tests",
+           "seclist-prep",
            "build",
            "dist",
            ".git",
            ".gitignore",
-           "Fuzzie.egg-info"
-           #"__pycache__"
+           "Fuzzie.egg-info",
+           "requirements.txt",
+           "core/core/api_discovery/testdata",
+           "core/core/corporafactory/test_*",
+           "core/core/.env"
            ]
        
     ),  #same as name
